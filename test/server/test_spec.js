78fba492ddff3471f995aca8b72bcc017bb1acd9
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-29 12:27:13 -0800

    Add global test hooks to recreate db before each test

--- test/server/test_spec.js
+++ test/server/test_spec.js
@@ -1,29 +1,62 @@
-var assert = require('assert');
+// globals
+var assert = require( 'assert' );
+var sequelize = require( 'sequelize' );
+var Users = require( '../../server/users/users' );
+var Sessions = require( '../../server/sessions/sessions' );
+var Sessions_Users = require( '../../server/sessions_users/sessions_users' );
+var Votes = require( '../../server/votes/votes' );
 
-describe('String#split with assert', function(){
-  it('should return an array', function(){
-    assert(Array.isArray('a,b,c'.split(',')));
+// setup
+// before();
+beforeEach( function() {
+  Votes.sync({force: true})
+  .then( function() {
+    Sessions_Users.sync({force: true})
+    .then( function() {
+      Sessions.sync({force: true})
+      .then( function() {
+        Votes.sync({force: true})
+        .then( function() {
+          Users.create({
+            username: 'Julie',
+            password: 'j'
+          })
+          .then( function() {
+            Users.create({
+              username: 'John',
+              password: 'j'
+            })
+            .then( function() {
+              Sessions.create({
+                sessionName: 'TestSession'
+              })
+              .then( function() {
+                Sessions_Users.create({
+                  session_id: 1,
+                  user_id: 1
+                })
+                .then( function() {
+                  Sessions_Users.create({
+                    session_id: 1,
+                    user_id: 2
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
   });
+  
+  // Wipe the database, and add in order:
+  // Julie (user id 1)
+  // John (user id 2)
+  // TestSession (session id 1)
+  // Sessions_Users w/ julie in testsession (sessions_users id 1)
+  // Sessions_Users w/ john in testsession (sessions_users id 2)
 });
 
-var expect = require('expect.js');
-var expected, current;
-
-before(function(){
- expected = ['a', 'b', 'c'];
-})
-describe('String#split with expect', function(){
- beforeEach(function(){
-   current = 'a,b,c'.split(',');
- })
- it('should return an array', function(){
-   expect(Array.isArray(current)).to.be.true;
- });
-
- it('should return the same array', function(){
-   expect(expected.length).to.equal(current.length);
-   for (var i=0; i<expected.length; i++) {
-     expect(expected[i]).equal(current[i]);
-   }
- })
-});
+// teardown
+// after();
+// afterEach();
